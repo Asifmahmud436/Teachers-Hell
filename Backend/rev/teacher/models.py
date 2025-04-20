d19697dfaadc2eb2b0a7fd6b9a80c641508ceefd
Abdullah Al Fahad <abdullah.al.fahad@outlook.com>
2025-04-20 14:41:38 +0600

    sentiment analysis added

--- Backend/rev/teacher/models.py
+++ Backend/rev/teacher/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from textblob import TextBlob
+
 class Faculty(models.Model):
     name = models.CharField(max_length=100)
     profile_picture_url = models.URLField(max_length=300)  # Store image link
@@ -12,3 +14,10 @@ class Review(models.Model):
     faculty = models.ForeignKey(Faculty, related_name='reviews', on_delete=models.CASCADE)
     rating = models.PositiveIntegerField()
     review_text = models.TextField()
+    sentiment = models.CharField(max_length=20, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.review_text:
+            analysis = TextBlob(self.review_text)
+            self.sentiment = 'positive' if analysis.sentiment.polarity > 0 else 'negative' if analysis.sentiment.polarity < 0 else 'neutral'
+        super().save(*args, **kwargs)
